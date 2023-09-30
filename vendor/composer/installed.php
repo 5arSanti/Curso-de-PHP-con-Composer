59698b247d0070622713e814168dca3de2bca944
5arSanti <santiari05@hotmail.com>
2023-09-30 08:44:34 -0500

    Composer basics explained

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '5arsanti/text',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a4afafafdccc538dedd4115b710841a127ad675b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '5arsanti/text' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a4afafafdccc538dedd4115b710841a127ad675b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
